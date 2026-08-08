4873a1ffaea3cfd29e19623e9326cc2b06a49943
Benoit Touron <benoit.touron@rockops.io>
2026-08-09 00:36:41 +0200

    wip killercoda 5

---
+++ 01-hello/background.sh
@@ -0,0 +1,40 @@
+echo "=== Cloning ostrich-sdk ==="
+
+export DEBIAN_FRONTEND=noninteractive
+export NEEDRESTART_MODE=a
+
+apt-get install -y -qq python3-venv
+
+mkdir -p /rockdemo/venv
+if [ ! -d /rockdemo/venv/bin ]; then
+    python3 -m venv /rockdemo/venv
+fi
+
+GLOW_VERSION=2.1.2
+
+mkdir -p /rockdemo
+cd /rockdemo
+
+if [ ! -f helm ]; then
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+    cp /usr/local/bin/helm .
+    rm get_helm.sh
+else
+    cp helm /usr/local/bin
+fi
+
+if [ ! -f glow_${GLOW_VERSION}_amd64.deb ]; then
+    wget https://github.com/charmbracelet/glow/releases/download/v$GLOW_VERSION/glow_${GLOW_VERSION}_amd64.deb
+fi
+
+dpkg -i glow_${GLOW_VERSION}_amd64.deb
+
+USER="USERNAME"
+TOKEN=$(test -f "/root/secret/github_read_osplates.txt" && cat /root/secret/github_read_osplates.txt)
+if [ -n "$TOKEN" ]; then
+    HELM_CONFIG_HOME="/root/.ostrich/helm/config" helm registry login ghcr.io -u "$USER" -p "$TOKEN"
+fi
+
+touch /tmp/finished
